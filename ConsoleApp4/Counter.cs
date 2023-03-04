055fcfdf21f4a7114902e770e5ee30c1ff15d95f
PavelReshetnev <pasha8804088@gmail.com>
2023-03-04 11:49:30 +0700

    Initial commit

---
+++ ConsoleApp4/Counter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace ConsoleApp4;
+
+public class Counter
+{
+    private int RNDnum;
+
+    public int Value {
+        get {
+            return RNDnum;
+        }
+    }
+
+    public Counter()
+    {
+        Random rnd = new Random();
+        this.RNDnum = rnd.Next(10, 100);
+    }
+    public Counter(int RNDnum)
+    {
+        this.RNDnum = RNDnum;
+    }
+
+    public void Increase()
+    {
+        RNDnum++;
+    }
+    public void Decrease()
+    {
+        RNDnum--;
+    }
+
+    public void Plus()
+    {
+        Console.WriteLine($"Работа счётчика...\n(+) -- прибавить");
+        for (int i = 0; i < 999; i++)
+        {
+            string kuda = Console.ReadLine();
+            if (kuda == "+")
+            {
+                Increase();
+                int res = Value;
+                Console.WriteLine(res);
+            }
+            else if (kuda == "")
+            {
+                break;
+            }
+        }
+        Console.WriteLine("Изменено...");
+    }
+
+    public void Minus()
+    {
+        Console.WriteLine($"Работа счётчика...\n(-) -- убавить");
+        for (int i = 0; i < 999; i++)
+        {
+            string kuda = Console.ReadLine();
+            if (kuda == "-")
+            {
+                Decrease();
+                int res = Value;
+                Console.WriteLine(res);
+            }else if (kuda == "")
+            {
+                break;
+            } 
+        }
+        Console.WriteLine("Изменено...");
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp4;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Counter chislo = new Counter();
+        
+        Console.WriteLine("1 - Прибавить в счётчику; 2 - убавить у счётчика; 3 - выход");
+        int a = Convert.ToInt32(Console.ReadLine());
+        switch (a) {
+            case 1:
+               chislo.Plus();
+                break;
+            case 2 :
+                chislo.Minus();
+                break; 
+            case 3:
+                break;
+        }
+    }
+}
